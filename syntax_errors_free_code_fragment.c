30c9251073cf6fec34e50277fc7b42c5dbc28432
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-10-30 15:12:24 +0100

    Fix syntax errors free code fragment style
    for readability

--- syntax_errors_free_code_fragment.c
+++ syntax_errors_free_code_fragment.c
@@ -48,8 +48,8 @@ void func3(int f3d){
 void main() {
     char *foo = "fooooooooooooooooooooooooooooooooooooooooooooooooooo";
     char *buffer = (char *)malloc(10 * sizeof(char));
+    
     strcpy(buffer, foo);
- 
     func1();
     func3(strlen(foo));
 }
